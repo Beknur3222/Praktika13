296b44db719296565b90feeee2737881d2ae9d64
Бекнур Саттар <www.asemush@gmail.com>
2023-11-23 14:58:32 +0600

    Добавьте файлы проекта.

---
+++ Exemple05/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple05
+{
+    class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+    }
+
+    class CompactDisk
+    {
+        public string Title { get; set; }
+        public List<Song> Songs { get; set; } = new List<Song>();
+    }
+
+    class MusicCatalog
+    {
+        private Hashtable catalog = new Hashtable();
+
+        public void AddDisk(string title)
+        {
+            CompactDisk disk = new CompactDisk { Title = title };
+            catalog.Add(title, disk);
+        }
+
+        public void RemoveDisk(string title)
+        {
+            catalog.Remove(title);
+        }
+
+        public void AddSong(string diskTitle, string songTitle, string artist)
+        {
+            if (catalog.ContainsKey(diskTitle))
+            {
+                CompactDisk disk = (CompactDisk)catalog[diskTitle];
+                disk.Songs.Add(new Song { Title = songTitle, Artist = artist });
+            }
+        }
+
+        public void RemoveSong(string diskTitle, string songTitle)
+        {
+            if (catalog.ContainsKey(diskTitle))
+            {
+                CompactDisk disk = (CompactDisk)catalog[diskTitle];
+                disk.Songs.RemoveAll(song => song.Title == songTitle);
+            }
+        }
+
+        public void DisplayCatalog()
+        {
+            foreach (DictionaryEntry entry in catalog)
+            {
+                CompactDisk disk = (CompactDisk)entry.Value;
+                Console.WriteLine($"Disk: {disk.Title}");
+                foreach (var song in disk.Songs)
+                {
+                    Console.WriteLine($"  Song: {song.Title} - {song.Artist}");
+                }
+            }
+        }
+
+        public void DisplayArtistSongs(string artist)
+        {
+            foreach (DictionaryEntry entry in catalog)
+            {
+                CompactDisk disk = (CompactDisk)entry.Value;
+                foreach (var song in disk.Songs)
+                {
+                    if (song.Artist == artist)
+                    {
+                        Console.WriteLine($"Disk: {disk.Title}, Song: {song.Title}");
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MusicCatalog catalog = new MusicCatalog();
+
+            catalog.AddDisk("Disk 1");
+            catalog.AddSong("Disk 1", "Шыда", "Мирас Жугунусов");
+            catalog.AddSong("Disk 1", "Цепи", "Скриптонит");
+
+            catalog.AddDisk("Disk 2");
+            catalog.AddSong("Disk 2", "Сен туралы", "Мирас Жугунусов");
+            catalog.AddSong("Disk 2", "Adjare Gudju", "Ирина Кайратовна");
+
+            catalog.DisplayCatalog();
+
+            catalog.RemoveSong("Disk 1", "Цепи");
+
+            Console.WriteLine("\nПосле удаления песни:\n");
+
+            catalog.DisplayCatalog();
+
+            Console.WriteLine("\nПоиск по истолнителю:\n");
+
+            catalog.DisplayArtistSongs("Мирас Жугунусов");
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Praktika13/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika13
+{
+    internal class Program
+    {
+        static void Exemple01()
+        {
+            List<int> numbers = new List<int> { 10, 5, 8, 20, 15 };
+
+            int secondMaxValue = numbers.OrderByDescending(x => x).Distinct().Skip(1).First();
+            int secondMaxIndex = numbers.FindIndex(x => x == secondMaxValue);
+
+            Console.WriteLine($"Position: {secondMaxIndex}, Value: {secondMaxValue}");
+
+            numbers = numbers.Where(x => x % 2 == 0).ToList();
+        }
+        static void Exemple02()
+        {
+            List<double> doubles = new List<double> { 10.5, 15.2, 8.7, 20.1, 12.3 };
+
+            double average = doubles.Average();
+            var result = doubles.Where(x => x > average);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Exemple03()
+        {
+            string inputFilePath = "input.txt";
+            string outputFilePath = "output.txt";
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+            Array.Reverse(lines);
+            File.WriteAllLines(outputFilePath, lines);
+        }
+        class Employee
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string Patronymic { get; set; }
+            public string Gender { get; set; }
+            public int Age { get; set; }
+            public decimal Salary { get; set; }
+        }
+        static void Exemple04()
+        {
+            string filePath = "employees.txt";
+            List<Employee> employees = File.ReadAllLines(filePath)
+                .Select(line => line.Split(','))
+                .Select(parts => new Employee
+                {
+                    LastName = parts[0],
+                    FirstName = parts[1],
+                    Patronymic = parts[2],
+                    Gender = parts[3],
+                    Age = int.Parse(parts[4]),
+                    Salary = decimal.Parse(parts[5])
+                })
+                .ToList();
+
+            var under10k = employees.Where(e => e.Salary < 10000);
+            var over10k = employees.Where(e => e.Salary >= 10000);
+
+            foreach (var employee in under10k)
+            {
+                Console.WriteLine($"{employee.LastName}, {employee.FirstName}, {employee.Patronymic}, {employee.Gender}, {employee.Age}, {employee.Salary}");
+            }
+
+            foreach (var employee in over10k)
+            {
+                Console.WriteLine($"{employee.LastName}, {employee.FirstName}, {employee.Patronymic}, {employee.Gender}, {employee.Age}, {employee.Salary}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Exemple01();
+            //Exemple02();
+            //Exemple03();
+            Exemple04();
+
+            Console.ReadKey();
+        }
+    }
+}
